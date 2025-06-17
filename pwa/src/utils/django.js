40d3bb2af8362b2270188342b6b22944eb37f6dc
José Gabriel Gruber <development@jgabrielgruber.dev>
2025-06-17 10:45:49 -0300

    implemented portal docker

--- pwa/src/utils/django.js
+++ pwa/src/utils/django.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_URL = 'http://localhost:8000'; // Switch to https://api.portal.com in prod
+
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'; // Switch to https://api.portal.com in prod
 
 // Axios instance with credentials for cookies
 const api = axios.create({

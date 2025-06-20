dd1391fc400d010b479e57e99a2e3300dfd64f1b
José Gabriel Gruber <development@jgabrielgruber.dev>
2025-06-20 20:54:10 -0300

    pwa: improved loading layout

--- pwa/src/components/protected_route.component.jsx
+++ pwa/src/components/protected_route.component.jsx
@@ -1,16 +1,23 @@
-import { Backdrop, CircularProgress } from "@mui/material"
+import { Backdrop, CircularProgress, Paper, ThemeProvider } from "@mui/material"
+import { GlobalStyles } from "@mui/styled-engine"
 import { Navigate, Outlet } from "react-router"
 
 import useAuthStore from "src/stores/auth.store"
+import { theme } from "src/theme"
 
 function ProtectedRoute() {
   const { user, isLoading } = useAuthStore()
 
   if (isLoading) {
     return (
-      <Backdrop open>
-        <CircularProgress color="primary" />
-      </Backdrop>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Paper elevation={24} sx={{ width: '100vw', height: '100vh' }}>
+          <Backdrop open>
+            <CircularProgress color="primary" disableShrink />
+          </Backdrop>
+        </Paper>
+      </ThemeProvider>
     ) // Show a loading state while checking auth
   }
 
